2cb751b6bb6abfd870f9d12708d3501f9b7d2bee
Achisha Saikia <achishasaikia21@gmail.com>
2023-06-09 14:27:10 -0400

    Create Klkfam.R

---
+++ Violin_plots/Klkfam.R
@@ -0,0 +1,22 @@
+setwd("~/Documents/Salivary glands/Mouse salivary glands/violin_plots")
+
+#Rcode for violin plots
+
+library(RColorBrewer)
+library(ggplot2)
+genes <- read.csv("klkfam.csv")
+head(genes)
+
+mycolors = c(brewer.pal(name="Dark2", n = 8), brewer.pal(name="Paired", n = 6))
+
+display.brewer.pal(11, "Paired")
+brewer.pal(11, "Paired")
+
+#tiff('test.tiff', units="in", width=5, height=4, res=300, compression = 'lzw')
+p=ggplot(genes, aes(x=Sex, y=log(Expression+1,10)))
+p+geom_violin(aes(col=Gene), alpha=0.3)+
+  facet_grid(Gene~Strain+Glands,scales="free") +
+  scale_color_manual(values = mycolors) +
+  geom_jitter(width=0.1)+
+  theme_light()
+#dev.off()
